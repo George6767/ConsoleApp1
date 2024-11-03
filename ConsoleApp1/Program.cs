2c45cf39b174e4fab5c2f8676264f1ac692ffe95
Ghost <Ghost@DESKTOP-H616JMJ>
2024-11-03 15:28:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Input deposit amont");
+double a = Convert.ToDouble(Console.ReadLine());
+double b,c;
+
+if (a < 100)
+{
+    b = a * 5 / 100;
+    c = a + b;
+    Console.WriteLine("Your deposit amount: " +a+ "\n" +
+        "Your percentage: " +b+ "\nDeposit amount with " +
+        "percentage is: " +c);
+}
+else if ((a>=100) && (a <= 200))
+{
+    b = a * 7 / 100;
+    c = a + b;
+    Console.WriteLine("Your deposit amount: " + a + "\n" +
+        "Your percentage: " +b+ "\nDeposit amount with " +
+        "percentage is: " + c);
+}
+else if (a > 200)
+{
+    b = a * 10 / 100;
+    c = a + b;
+    Console.WriteLine("Your deposit amount: " + a + "\n" +
+        "Your percentage: " +b+ "\nDeposit amount with " +
+        "percentage is: " +c);
+}
+else
+{
+    Console.WriteLine("Input another deposit amont");
+}
